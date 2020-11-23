bb9ee8b61b098f1885756cdcebb6263775cbd429
helloye <hello.paingy@gmail.com>
2020-11-23 00:52:06 -0500

    print line comment code for demo

--- akka-actor-demo/src/main/scala/com/example/demo.scala
+++ akka-actor-demo/src/main/scala/com/example/demo.scala
@@ -15,6 +15,8 @@ object Main extends App {
   val system = ActorSystem("DemoSystem")
   val helloActor = system.actorOf(Props[HelloActor], name = "helloactor")
 
+  // println('A')
+
   helloActor ! "hello" // String "hello"
   helloActor ! "Some other message other than hello" // String
   helloActor ! 1234 // Int
