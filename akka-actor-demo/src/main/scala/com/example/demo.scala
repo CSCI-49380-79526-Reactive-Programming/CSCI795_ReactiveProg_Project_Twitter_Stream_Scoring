02cfbaa34b0049f62e82e7aae725d30948c027c8
helloye <hello.paingy@gmail.com>
2020-11-23 00:48:32 -0500

    adding akka demo slides and code

---
+++ akka-actor-demo/src/main/scala/com/example/demo.scala
@@ -0,0 +1,24 @@
+import akka.actor.Actor
+import akka.actor.ActorSystem
+import akka.actor.Props
+
+class HelloActor extends Actor {
+  def receive = {
+    case "hello"  => println("hello back at you")
+    case s:String => println("Received message: " + s)
+    case n:Int    => println("Received integer: " + n)
+    case _        => println("huh?")
+  }
+}
+
+object Main extends App {
+  val system = ActorSystem("DemoSystem")
+  val helloActor = system.actorOf(Props[HelloActor], name = "helloactor")
+
+  helloActor ! "hello" // String "hello"
+  helloActor ! "Some other message other than hello" // String
+  helloActor ! 1234 // Int
+  helloActor ! 'c' // Char
+
+}
+
