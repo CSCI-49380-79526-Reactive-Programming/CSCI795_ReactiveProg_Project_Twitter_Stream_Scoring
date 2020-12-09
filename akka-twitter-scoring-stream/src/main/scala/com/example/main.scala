c30f4d814bb2bc6a54975ab7dee0c0ba85ea0618
Alex Washburn <github@recursion.ninja>
2020-12-09 11:07:55 -0500

    Adding Critic actor for statefully maintaining scores

--- akka-twitter-scoring-stream/src/main/scala/com/example/main.scala
+++ akka-twitter-scoring-stream/src/main/scala/com/example/main.scala
@@ -1,4 +1,5 @@
 import akka.actor.Actor
+import akka.actor.ActorRef
 import akka.actor.ActorSystem
 import akka.actor.Props
 import com.github.tototoshi.csv._
@@ -6,19 +7,84 @@ import java.io.File
 import java.time._
 
 // Twitter4s streaming library
+import com.danielasfregola.twitter4s.entities._
 import com.danielasfregola.twitter4s.TwitterRestClient
 import com.danielasfregola.twitter4s.TwitterStreamingClient
 
 class HelloActor extends Actor {
   def receive = {
     case "hello"  => println("hello back at you")
+                     println(sender)
+
     case s:String => println("Received message: " + s)
     case n:Int    => println("Received integer: " + n)
     case _        => println("huh?")
   }
 }
 
-class Politician( val name       : String
+
+class Critic extends Actor {
+
+  val scores = new scala.collection.mutable.HashMap[String, TwitterScoring]
+
+  def receive = {
+    case (politician: String, tweet: Tweet) =>
+      if (scores(politician).update(tweet)) {
+        // TODO update TUI/GUI
+      }
+  }
+}
+
+
+class TwitterScoring {
+  // Two sets of tweet IDs
+  val disputed   = new scala.collection.mutable.HashSet[Long]
+  val undisputed = new scala.collection.mutable.HashSet[Long]
+
+  def update(tweet : Tweet) : Boolean = {
+    val id      = tweet.id
+    val flagged = !tweet.withheld_in_countries.isEmpty
+    if (disputed contains id) {
+      // We already processed this tweet as disputed
+      if (!flagged) {
+        // However it's status has changed to undisputed
+        disputed   - id
+        undisputed + id
+        return true
+       }
+       // And no change is required
+    }
+    else if (undisputed contains id) {
+      // We processed this tweet as undisputed,
+      if (flagged) {
+        // However it's status has changed to disputed
+        disputed + id
+        undisputed - id
+        return true
+      }
+      // And no change is required
+    }
+    else {
+      // We have *not* processed this tweet
+      if (flagged) {
+        disputed + id
+      }
+      else {
+        undisputed - id
+      }
+      return true
+    }
+    return false
+  }
+  
+  def pinochiccoScore() : Double = {
+    disputed.size / (disputed.size + undisputed.size)
+  }
+
+}
+
+class Politician( val updater    : ActorRef
+                , val name       : String
                 , val party      : String
                 , val state      : String
                 , val twitter    : String
@@ -43,11 +109,12 @@ object Main extends App {
   val senatorsFile = "us-senate.csv" 
   val system       = ActorSystem("DemoSystem")
   val helloActor   = system.actorOf(Props[HelloActor], name = "helloactor")
+  val critic       = system.actorOf(Props[Critic], name = "critic")
 
   val stream  = CSVReader.open(new File(senatorsFile)).iterator
   stream.next // Drop the header row from the stream iterator
   // Add all senators as actors to our system
-  stream foreach addPolitician(system)
+  stream foreach addPolitician(system)(critic)
 
  /*
     Import works ok, uncommenting it will error as we need to provide
@@ -71,7 +138,7 @@ object Main extends App {
   helloActor ! helloActor
   println('F')
 
-  def addPolitician(system : ActorSystem)(dataRow : Seq[String]) : Unit = {
+  def addPolitician(system : ActorSystem)(updater : ActorRef )(dataRow : Seq[String]) : Unit = {
     // Convert Seq to VEctor for more efficient random access
     val vec     = dataRow.toVector
     // Extract relevent fields from the row
@@ -83,7 +150,8 @@ object Main extends App {
     val begin   = Instant.parse(vec(28) + "T00:00:00.00Z")
     val finish  = Instant.parse(vec(29) + "T00:00:00.00Z")
     // Add a new Politician actor from the row data
-    system.actorOf(Props(new Politician(name, party, state, twitter, begin, finish)), name = id)
+    println("Adding " + name)
+    system.actorOf(Props(new Politician(updater, name, party, state, twitter, begin, finish)), name = id)
   }
 
 }
